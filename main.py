d0adba1c70531cb2f3f8da295f38836d343de6ed
Medina <73224995+MedinaIsabekova@users.noreply.github.com>
2020-12-16 18:09:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ def update_listbox():
 
 def clear_listbox():
     lb_tasks.delete(0, "end")
+    
+    
+def add_task():
+    task = txt_input.get()
+    if task != "":
+        tasks.append(task)
+        update_listbox()
+    else:
+        lbl_display["text"] = "Please enter a task!"
+        txt_input.delete(0, "end")
 
 lb_tasks = tkinter.Listbox(root, width=35, font=('Times New Roman', 12, 'bold'))
 lb_tasks.grid(row=2, column=1, rowspan=10)
