103fd52b5e31b43485f5df9b71efde87c45b99f2
junesqo <junesqo@gmail.com>
2020-12-16 16:31:22 +0600

    Nurel: add listbox

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ import random
 import tkinter.messagebox
 
 root = tkinter.Tk()
+
+
+root.geometry("600x360")
+root.configure(bg="coral", padx=20, pady=20)
+root.title("ToDo List")
+
+tasks = []
+
+
+def update_listbox():
+    clear_listbox()
+    for task in tasks:
+        lb_tasks.insert("end", task)
+
+def clear_listbox():
+    pass
+
+lb_tasks = tkinter.Listbox(root, width=35, font=('Times New Roman', 12, 'bold'))
+lb_tasks.grid(row=2, column=1, rowspan=10)
+
+root.mainloop()
