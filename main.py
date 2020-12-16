4ba6dc3759970e2b9035572bc393d6e6cd2d980f
Medina <73224995+MedinaIsabekova@users.noreply.github.com>
2020-12-16 18:09:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def update_listbox():
         lb_tasks.insert("end", task)
 
 def clear_listbox():
-    pass
+    lb_tasks.delete(0, "end")
 
 lb_tasks = tkinter.Listbox(root, width=35, font=('Times New Roman', 12, 'bold'))
 lb_tasks.grid(row=2, column=1, rowspan=10)
