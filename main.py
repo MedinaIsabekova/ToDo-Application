ffb592449d2bc123e9eeedb5f58af1868e6da68e
junesqo <junesqo@gmail.com>
2020-12-20 02:20:31 +0600

    frontend upgrade

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root = tkinter.Tk()
 
 
 root.geometry("600x350")
-root.configure(bg="coral", padx=20, pady=20)
+root.configure(bg="#cdcdcd", padx=20, pady=20)
 root.title("ToDo List")
 
 tasks = []
@@ -52,30 +52,30 @@ def show_number_of_tasks():
     msg = "Number of tasks: %s" %number_of_tasks 
     lbl_display["text"] = msg 
 
-lbl_display = tkinter.Label(root, text="Medina & Nurel ToDo List", bg="brown", fg="coral", font=('Times New Roman', 13, 'bold')) 
+lbl_display = tkinter.Label(root, text="Medina & Nurel ToDo List", bg="teal", fg="white", font=('Arial', 13, 'bold')) 
 lbl_display.grid(row=0, column=1) 
 
 txt_input = tkinter.Entry(root, width=35, font=('Courier New', 12, 'bold')) 
 txt_input.grid(row=1, column=1) 
 
 btn_add_task = tkinter.Button(root, text="Add Task",
-                              fg="coral", bg="brown", width=15, command=add_task,
-                              font=('Times New Roman', 10, 'bold'))
+                              fg="white", bg="green", width=15, command=add_task,
+                              font=('Arial', 10, 'bold'))
 btn_add_task.grid(row=1, column=0, padx=20, pady=(0, 15))
 
-btn_del_all = tkinter.Button(root, text="Delete All", fg="coral", bg="brown", width=15, command=del_all, font=('Times New Roman', 10, 'bold'))
+btn_del_all = tkinter.Button(root, text="Delete All", fg="white", bg="teal", width=15, command=del_all, font=('Arial', 10, 'bold'))
 btn_del_all.grid(row=2, column=0)
 
-btn_del_one = tkinter.Button(root, text="Delete Selected", fg="#FF0101", bg="#000000", width=15, command=del_one, font=('Arial', 10, 'bold'))
-btn_del_one.grid(row=3, column=0, pady=(0, 15))
+btn_del_one = tkinter.Button(root, text="Delete Selected", fg="white", bg="teal", width=15, command=del_one, font=('Arial', 10, 'bold'))
+btn_del_one.grid(row=4, column=0, pady=(0, 15))
 
-lb_tasks = tkinter.Listbox(root, width=35, font=('Times New Roman', 12, 'bold'))
+lb_tasks = tkinter.Listbox(root, width=35, font=('Arial', 12, 'bold'))
 lb_tasks.grid(row=2, column=1, rowspan=10)
 
-btn_number_of_tasks = tkinter.Button(root, text="Number of Tasks", fg="coral", bg="brown", width=15, command=show_number_of_tasks, font=('Times New Roman', 10, 'bold')) 
-btn_number_of_tasks.grid(row=7, column=0)
+btn_number_of_tasks = tkinter.Button(root, text="Number of Tasks", fg="white", bg="teal", width=15, command=show_number_of_tasks, font=('Arial', 10, 'bold')) 
+btn_number_of_tasks.grid(row=5, column=0)
 
-btn_exit = tkinter.Button(root, text="Exit", fg="coral", bg="brown", width=15, command=exit, font=('Times New Roman', 10, 'bold'))
+btn_exit = tkinter.Button(root, text="Exit", fg="white", bg="red", width=15, command=exit, font=('Arial', 10, 'bold'))
 btn_exit.grid(row=8, column=0, pady=(15, 0))
 
 root.mainloop()
